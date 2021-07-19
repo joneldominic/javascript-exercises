abccfd3787c76a7d3041ec65a78adbef4a58a14b
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-07-19 18:57:11 +0800

    CHORE: Enahnce leapYears solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,10 +1,12 @@
 const leapYears = function(year) {
 
-    if((year%4===0 && year%100!==0) || 
-        (year%100===0 && year%400===0)) 
-        return true;
+    // if((year%4===0 && year%100!==0) || 
+    //     (year%100===0 && year%400===0)) 
+    //     return true;
 
-    return false;
+    // return false;
+
+    return year%4===0 && (year%100!=0 || year%400===0);
 };
 
 module.exports = leapYears;
