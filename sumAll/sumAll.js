ab63b805f4356cf86c0a36b1d081f01e2d25c4e2
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-07-19 16:37:28 +0800

    CHORE: Implemented sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let min, max;
+    let sum=0;
 
+    if(args.length!==2) return 'ERROR';
+
+    for(let i=0; i<args.length; i++) {
+        if(typeof args[i]!=='number' || args[i]<0)
+            return 'ERROR';
+    }
+
+    [min, max] = args;
+    if(min>max) 
+        [min, max] = [max, min];
+
+    for(let i=min; i<=max; i++)
+        sum+=i;
+    
+    return sum;
 };
 
 module.exports = sumAll;
