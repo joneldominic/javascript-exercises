4aa760bc914610a296380c159101ffd8d5741d67
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-07-19 19:10:23 +0800

    CHORE: Implemented tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celcius = (fahrenheit-32)*(5/9);
+  return roundNumber(celcius);
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  fahrenheit = celcius*(9/5)+32;
+  return roundNumber(fahrenheit);
 };
+
+const roundNumber = function(number) {
+  return Math.round(number*10)/10;
+}
 
 module.exports = {
   ftoc,
