d6ded6c29c04f15024dd1ab285ea6e0811e35156
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-07-19 14:11:02 +0800

    CHORE: Implemented re[eatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(inputString, times) {
+    let result = ''
 
+    if(times<0) return 'ERROR';
+
+    for(let i=0; i<times; i++) {
+        result+=inputString;
+    }
+
+    return result;
 };
 
 module.exports = repeatString;
