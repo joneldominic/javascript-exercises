72a16c2e412ce518c7551be15aef8ce0ccf7fcd5
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-07-19 14:47:31 +0800

    CHORE: Implement removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...toRemove) {
+    let resultArray = inputArray.slice();
 
+    toRemove.forEach(function(currValue){
+        const indexToRemove = resultArray.indexOf(currValue);
+        if(indexToRemove > -1) resultArray.splice(indexToRemove, 1);
+    });
+
+    return resultArray
 };
 
 module.exports = removeFromArray;
